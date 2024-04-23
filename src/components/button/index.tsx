8ef89bfdc144588fb04c8cc5056338effc7113b5
Newton Duarte <newton_@hotmail.com>
2024-04-22 21:37:23 -0300

    feat: add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,106 @@
+import { styled } from '@/styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  borderRadius: '$md',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  boxSizing: 'border-box',
+  padding: '0 $4',
+  cursor: 'pointer',
+
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$primary',
+
+        '&:not(:disabled):hover': {
+          background: '$primary_dark',
+        },
+
+        '&:disabled': {
+          backgroundColor: '$gray200',
+        },
+      },
+
+      secondary: {
+        background: '$secondary',
+
+        '&:not(:disabled):hover': {
+          background: '$gray600',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        background: '$tertiary',
+
+        '&:not(:disabled):hover': {
+          color: '$secondary',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+
+    size: {
+      sm: {
+        height: 48,
+      },
+
+      md: {
+        height: 64,
+      },
+
+      lg: {
+        height: 72,
+      },
+    },
+
+    color: {
+      purple: {
+        color: '$purple100 !important',
+        '&:not(:disabled):hover': {
+          color: '$white !important',
+        },
+      },
+      default: {
+        color: 'inherit',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+    color: 'default',
+  },
+})
