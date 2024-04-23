b66e14ade5498b410d3636e1b72e3f0e2ad484de
Newton Duarte <newton_@hotmail.com>
2024-04-22 21:37:51 -0300

    feat: add text component

---
+++ src/components/text/index.tsx
@@ -0,0 +1,29 @@
+import { styled } from '@/styles'
+import { ComponentProps, ElementType } from 'react'
+
+export const Text = styled('p', {
+  margin: 0,
+  color: '$white',
+  lineHeight: '$base',
+
+  variants: {
+    size: {
+      xs: { fontSize: '$xs' },
+      sm: { fontSize: '$sm' },
+      md: { fontSize: '$md' },
+      lg: { fontSize: '$lg' },
+      xl: { fontSize: '$xl' },
+      '2xl': { fontSize: '$2xl' },
+    },
+  },
+
+  defaultVariants: {
+    size: 'md',
+  },
+})
+
+export interface TextProps extends ComponentProps<typeof Text> {
+  as?: ElementType
+}
+
+Text.displayName = 'Text'
