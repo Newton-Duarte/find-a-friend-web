26a2dc9f22dc50386bd83d42133e6174c97c48f1
Newton Duarte <newton_@hotmail.com>
2024-04-22 21:37:13 -0300

    style: add global styles with stitches

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
-import './globals.css'
+import { globalStyles } from '@/styles/global'
+import { getCssText } from '@/styles'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -10,13 +11,21 @@ export const metadata: Metadata = {
     'Leve a felicidade para o seu lar. Encontre o animal de estimação ideal para seu estilo de vida!',
 }
 
+globalStyles()
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
+      <head>
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </head>
       <body className={nunito.className}>{children}</body>
     </html>
   )
---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  html: {
+    height: '100dvh',
+  },
+
+  body: {
+    height: '100dvh',
+    color: '$white',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Nunito Sans, sans-serif',
+    fontWeight: 400,
+  },
+
+  '#__next': {
+    height: '100dvh',
+  },
+})
