513368eac2216c8d49f509491745bd53282b512c
Newton Duarte <newton_@hotmail.com>
2024-04-23 21:37:39 -0300

    feat: add pet utils

---
+++ src/utils/pet-utils.ts
@@ -0,0 +1,27 @@
+export const petAges = [
+  { id: 1, value: 'young', label: 'Filhote' },
+  { id: 2, value: 'adult', label: 'Adulto' },
+  { id: 3, value: 'aged', label: 'Idoso' },
+]
+
+export const petSizes = [
+  { id: 1, value: 'small', label: 'Pequeno' },
+  { id: 2, value: 'medium', label: 'Médio' },
+  { id: 3, value: 'large', label: 'Grande' },
+]
+
+export const petEnergyLevels = [
+  { id: 1, value: 'low', label: 'Baixa' },
+  { id: 2, value: 'medium', label: 'Média' },
+  { id: 3, value: 'high', label: 'Alta' },
+]
+
+export const petIndependenceLevels = [
+  { id: 1, value: 'low', label: 'Baixa' },
+  { id: 2, value: 'medium', label: 'Média' },
+  { id: 3, value: 'high', label: 'Alta' },
+]
+
+export const petTypes = [
+  { id: 1, value: 'cats-dogs', label: 'Gatos e Cachorros' },
+]
