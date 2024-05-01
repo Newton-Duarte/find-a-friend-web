0e8b5959f75d0e54e6455e57af966725c249f2e2
Newton Duarte <newton_@hotmail.com>
2024-05-01 11:02:33 -0300

    feat: add get pets api function and hook

--- src/api/get-pets.ts
+++ src/api/get-pets.ts
@@ -1,9 +1,8 @@
-import { api } from '@/data/api'
+import { axiosApi } from '@/lib/axios'
 import { capitalizeWord } from '@/utils/capitalize-word'
 
 export async function getPets(city: string) {
-  const response = await api(`/pets?city=${capitalizeWord(city)}`)
-  const data = await response.json()
+  const response = await axiosApi.get(`/pets?city=${capitalizeWord(city)}`)
 
-  return data
+  return response.data
 }
--- src/hooks/usePets.ts
+++ src/hooks/usePets.ts
@@ -1,6 +1,20 @@
-import { useContext } from 'react'
-import { PetsContext } from '@/contexts/pet-context'
+import { useSearchParams } from 'next/navigation'
+import { useQuery } from '@tanstack/react-query'
+import { getPets } from '@/api/get-pets'
+import { Pet } from '@/data/types/pet'
+
+interface UsePetsResponse {
+  pets: Pet[]
+}
 
 export function usePets() {
-  return useContext(PetsContext)
+  const searchParams = useSearchParams()
+  const city = searchParams.get('city')
+
+  return useQuery<UsePetsResponse>({
+    queryFn: () => getPets(city || ''),
+    queryKey: ['pets', city],
+    staleTime: 1000 * 60 * 1, // 1 minute
+    refetchOnWindowFocus: false,
+  })
 }
