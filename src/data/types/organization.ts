de412600cbe3d62b10cfd3d8ffc1e455567b3a15
Newton Duarte <newton_@hotmail.com>
2024-05-01 11:02:16 -0300

    chore: add organization type

---
+++ src/data/types/organization.ts
@@ -0,0 +1,14 @@
+export type Organization = {
+  id: string
+  name: string
+  owner_name: string
+  email: string
+  phone: string
+  description: string
+  street_name: string
+  street_number: string
+  neighborhood: string
+  city: string
+  postal_code: string
+  created_at: string
+}
