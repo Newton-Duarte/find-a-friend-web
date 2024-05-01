68332b68cdb17e6b30b904e322a5c823679537df
Newton Duarte <newton_@hotmail.com>
2024-05-01 11:03:20 -0300

    feat: implement pets page

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -9,68 +9,65 @@ import {
 } from '@/utils/pet-utils'
 import { Logo } from '@/components/logo'
 import { SelectCity } from './select-city'
-import { PetsContextProvider } from '@/contexts/pet-context'
 import { PetsList } from './pets-list'
 
 import * as S from './styles'
 
 export default async function Pets() {
   return (
-    <PetsContextProvider>
-      <S.Wrapper>
-        <S.Sidebar>
-          <S.SidebarHeader>
-            <Logo width={180} height={45} />
-            <SelectCity />
-          </S.SidebarHeader>
-          <S.SidebarFilters>
-            <Text size="2xl" as="h4">
-              Filtros
-            </Text>
-            <S.FormGroup>
-              <Text size="sm">Idade</Text>
-              <Select
-                options={petAges}
-                defaultValue={petAges[0].value}
-                variant="primary"
-              />
-            </S.FormGroup>
-            <S.FormGroup>
-              <Text size="sm">Nível de energia</Text>
-              <Select
-                options={petEnergyLevels}
-                defaultValue={petEnergyLevels[0].value}
-                variant="primary"
-              />
-            </S.FormGroup>
-            <S.FormGroup>
-              <Text size="sm">Porte do animal</Text>
-              <Select
-                options={petSizes}
-                defaultValue={petSizes[0].value}
-                variant="primary"
-              />
-            </S.FormGroup>
-            <S.FormGroup>
-              <Text size="sm">Nível de independência</Text>
-              <Select
-                options={petIndependenceLevels}
-                defaultValue={petIndependenceLevels[0].value}
-                variant="primary"
-              />
-            </S.FormGroup>
-          </S.SidebarFilters>
-        </S.Sidebar>
-        <S.Content>
-          <S.Header>
-            <Text size="2xl">
-              Encontre <strong>324 amigos</strong> na sua cidade
-            </Text>
-            <Select options={petTypes} defaultValue={petTypes[0].value} />
-          </S.Header>
-          <PetsList />
-        </S.Content>
-      </S.Wrapper>
-    </PetsContextProvider>
+    <S.Wrapper>
+      <S.Sidebar>
+        <S.SidebarHeader>
+          <Logo width={180} height={45} />
+          <SelectCity />
+        </S.SidebarHeader>
+        <S.SidebarFilters>
+          <Text size="2xl" as="h4">
+            Filtros
+          </Text>
+          <S.FormGroup>
+            <Text size="sm">Idade</Text>
+            <Select
+              options={petAges}
+              defaultValue={petAges[0].value}
+              variant="primary"
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <Text size="sm">Nível de energia</Text>
+            <Select
+              options={petEnergyLevels}
+              defaultValue={petEnergyLevels[0].value}
+              variant="primary"
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <Text size="sm">Porte do animal</Text>
+            <Select
+              options={petSizes}
+              defaultValue={petSizes[0].value}
+              variant="primary"
+            />
+          </S.FormGroup>
+          <S.FormGroup>
+            <Text size="sm">Nível de independência</Text>
+            <Select
+              options={petIndependenceLevels}
+              defaultValue={petIndependenceLevels[0].value}
+              variant="primary"
+            />
+          </S.FormGroup>
+        </S.SidebarFilters>
+      </S.Sidebar>
+      <S.Content>
+        <S.Header>
+          <Text size="2xl">
+            Encontre <strong>324 amigos</strong> na sua cidade
+          </Text>
+          <Select options={petTypes} defaultValue={petTypes[0].value} />
+        </S.Header>
+        <PetsList />
+      </S.Content>
+    </S.Wrapper>
   )
 }
--- src/app/pets/pets-list.tsx
+++ src/app/pets/pets-list.tsx
@@ -1,36 +1,47 @@
 'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+
 import { usePets } from '@/hooks/usePets'
+import { Text } from '@/components/text'
 
 import * as S from './styles'
 
 export function PetsList() {
-  const { pets } = usePets()
+  const { data, isLoading, isError, error } = usePets()
 
   return (
     <S.PetsList>
-      {pets.map((pet) => (
-        <Link key={pet.id} href={`/pets/${pet.id}`}>
-          <S.Pet>
-            <Image
-              src="/images/pets/alfredo.png"
-              width={274}
-              height={135}
-              alt=""
-            />
-            <S.IconBox>
+      {isLoading ? (
+        <p>Carregando...</p>
+      ) : isError ? (
+        <p>Erro: {error.message}</p>
+      ) : data?.pets?.length ? (
+        data.pets.map((pet) => (
+          <Link key={pet.id} href={`/pets/${pet.id}`}>
+            <S.Pet>
               <Image
-                src="/images/logo-icon.png"
-                width={15}
-                height={15}
+                src="/images/pets/alfredo.png"
+                width={274}
+                height={135}
                 alt=""
               />
-            </S.IconBox>
-            <Text size="lg">{pet.name}</Text>
-          </S.Pet>
-        </Link>
-      ))}
+              <S.IconBox>
+                <Image
+                  src="/images/logo-icon.png"
+                  width={15}
+                  height={15}
+                  alt=""
+                />
+              </S.IconBox>
+              <Text size="lg">{pet.name}</Text>
+            </S.Pet>
+          </Link>
+        ))
+      ) : (
+        <p>Nenhum pet encontrado...</p>
+      )}
     </S.PetsList>
   )
 }
